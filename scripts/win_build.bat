691f90e9fa2ab20feb168cc020e713469c8b538b
setteZ <furgorsin@gmail.com>
2024-08-30 23:22:16 +0200

    win_build improved

--- scripts/win_build.bat
+++ scripts/win_build.bat
@@ -3,9 +3,8 @@ python -m venv --clear --upgrade-deps .venv_build
 call .venv_build\Scripts\activate
 pip install .
 pip install .[build]
-if exist "build\" rm -r build\
 if exist "dist\" rm -r dist\
-pyinstaller --icon=.\media\yacc.ico --onefile .\yacc\yacc.py -p .\yacc
+pyinstaller --clean --windowed --noupx --icon=.\media\yacc.ico --onefile .\yacc\yacc.py -p .\yacc
 if %errorlevel% equ 0 (
     rm -r build\
 )
